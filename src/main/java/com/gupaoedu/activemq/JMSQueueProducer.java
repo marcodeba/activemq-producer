bf66c4a9aa6b4c02657096734846c9622278a1b0
panning255 <panning255@jk.cn>
2019-01-10 22:40:00 +0800

    activemq-producer

--- src/main/java/com/gupaoedu/activemq/JMSQueueProducer.java
+++ src/main/java/com/gupaoedu/activemq/JMSQueueProducer.java
@@ -4,11 +4,6 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
 
-/**
- * 腾讯课堂搜索 咕泡学院
- * 加群获取视频：608583947
- * 风骚的Michael 老师
- */
 public class JMSQueueProducer {
 
     public static void main(String[] args) {
@@ -21,7 +16,7 @@ public class JMSQueueProducer {
             connection.start();
 
             Session session = connection.createSession
-                    (Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+                    (Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
             //创建目的地
             Destination destination = session.createQueue("myQueue");
             //创建发送者
@@ -35,7 +30,7 @@ public class JMSQueueProducer {
                 //Text   Map  Bytes  Stream  Object
                 producer.send(message);
             }
-//            session.commit();
+            session.commit();
             session.close();
         } catch (JMSException e) {
             e.printStackTrace();
--- src/main/java/com/gupaoedu/activemq/JMSTopicProducer.java
+++ src/main/java/com/gupaoedu/activemq/JMSTopicProducer.java
@@ -4,20 +4,14 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
 
-/**
- * 腾讯课堂搜索 咕泡学院
- * 加群获取视频：608583947
- * 风骚的Michael 老师
- */
 public class JMSTopicProducer {
 
     public static void main(String[] args) {
         ConnectionFactory connectionFactory =
                 new ActiveMQConnectionFactory
-                        ("tcp://192.168.11.153:61616");
+                        ("tcp://localhost:61616");
         Connection connection = null;
         try {
-
             connection = connectionFactory.createConnection();
             connection.start();
 
@@ -30,14 +24,11 @@ public class JMSTopicProducer {
 
             //创建需要发送的消息
             TextMessage message = session.createTextMessage("vip的上课时间是：周三、周六、周日");
-
             //Text   Map  Bytes  Stream  Object
-
             producer.send(message);
 
+            // 确认消息发送到消息中间件上
             session.commit();
-            session.rollback();
-
             session.close();
         } catch (JMSException e) {
             e.printStackTrace();
